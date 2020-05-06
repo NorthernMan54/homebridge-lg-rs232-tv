b84c53b02b10b8c7d334070be3d3d70a8cedcfc6
NorthernMan54 <buyminivan-northern@yahoo.ca>
2020-05-06 18:55:52 -0400

    WIP

--- test.js
+++ test.js
@@ -8,7 +8,7 @@ port.powerStatus(function(err, data) {
   if (err) {
     console.error(err.message);
   } else {
-    console.log("Power", data, ascii_to_hexa(data));
+    console.log("Power", data, asciiToHexa(data));
   }
 });
 
@@ -16,17 +16,15 @@ port.inputStatus(function(err, data) {
   if (err) {
     console.error(err.message);
   } else {
-    console.log("Power", data, ascii_to_hexa(data));
+    console.log("Input", data, asciiToHexa(data));
   }
 });
 
-function ascii_to_hexa(str)
-  {
-	var arr1 = [];
-	for (var n = 0, l = str.length; n < l; n ++)
-     {
-		var hex = Number(str.charCodeAt(n)).toString(16);
-		arr1.push(hex);
-	 }
-	return arr1.join('');
-   }
+function asciiToHexa(str) {
+  var arr1 = [];
+  for (var n = 0, l = str.length; n < l; n++) {
+    var hex = Number(str.charCodeAt(n)).toString(16);
+    arr1.push(hex);
+  }
+  return arr1.join('');
+}
