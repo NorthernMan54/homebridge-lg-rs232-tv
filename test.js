6dc28c4ba9d8a019709e92a53a2efa3c96d60b96
NorthernMan54 <buyminivan-northern@yahoo.ca>
2020-05-06 18:37:39 -0400

    WIP

--- test.js
+++ test.js
@@ -1,7 +1,13 @@
-var LgSerialPort = require('./lib/LgSerialPort.js');
+var LgSerialPort = require('./lib/LgSerialPort').LgSerialPort;
 
 var port = new LgSerialPort({
   port: "/dev/ttyUSB0"
 });
 
-port.powerStatus();
+port.powerStatus(function(err, data) {
+  if (err) {
+    console.error(err.message);
+  } else {
+    console.log(data);
+  }
+});
