b9c13f15b4c176c5a6cf7870a5966c6225b75fb3
NorthernMan54 <buyminivan-northern@yahoo.ca>
2020-05-10 19:35:01 -0400

    WIP

--- index.js
+++ index.js
@@ -368,23 +368,27 @@ LgTv.prototype.pollStatus = function() {
   debug("pollStatus: start", this.device.name);
 
   this.serialPort.powerStatus(function(err, response) {
-    debug("powerStatus: Response", err.message, response);
+    debug("powerStatus: Response", response);
     // a 00 OK01
     // debug("powerStatus", this.accessory.getService(this.device.name).getCharacteristic(Characteristic.Active));
     if (err) {
+      debug("powerStatus: Response", err.message);
       this.accessory.getService(this.device.name).getCharacteristic(Characteristic.Active).updateValue(err);
     } else if (response.substring(7, 9) === "01") {
+      debug("powerStatus: Response", response);
       this.accessory.getService(this.device.name).getCharacteristic(Characteristic.Active).updateValue(1);
     } else {
+      debug("powerStatus: Response", response);
       this.accessory.getService(this.device.name).getCharacteristic(Characteristic.Active).updateValue(0);
     }
   }.bind(this));
 
   this.serialPort.inputStatus(function(err, response) {
-    debug("inputStatus: Response", err.message, response);
     if (err) {
+      debug("inputStatus: Response", err.message, response);
       this.accessory.getService(this.device.name).getCharacteristic(Characteristic.ActiveIdentifier).updateValue(err);
     } else if (response.substring(7, 9) === "00") {
+      debug("inputStatus: Response", response);
       // Watching TV
       this.serialPort.channelStatus(function(err, response) {
         debug("channelStatus: Response", err, response);
@@ -392,6 +396,7 @@ LgTv.prototype.pollStatus = function() {
         this.accessory.getService(this.device.name).getCharacteristic(Characteristic.ActiveIdentifier).updateValue(_getIdentifier(this.inputs, _decodeChannel(response)));
       }.bind(this));
     } else {
+      debug("inputStatus: Response", response);
       // debug("ActiveIdentifier: Input Response", this.inputs, input);
       debug("ActiveIdentifier: Input Response", _getIdentifier(this.inputs, response.substring(7, 9)));
       this.accessory.getService(this.device.name).getCharacteristic(Characteristic.ActiveIdentifier).updateValue(_getIdentifier(this.inputs, response.substring(7, 9)));
