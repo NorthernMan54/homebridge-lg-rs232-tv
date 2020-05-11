753a3be8dff3d2b5a31e1cd4263ed79773902fe9
NorthernMan54 <buyminivan-northern@yahoo.ca>
2020-05-10 20:36:19 -0400

    WIP

--- index.js
+++ index.js
@@ -392,8 +392,12 @@ LgTv.prototype.pollStatus = function() {
       // Watching TV
       this.serialPort.channelStatus(function(err, response) {
         debug("channelStatus: Response", err, response);
-        debug("ActiveIdentifier: Channel Response", _getIdentifier(this.inputs, _decodeChannel(response)));
-        this.accessory.getService(this.device.name).getCharacteristic(Characteristic.ActiveIdentifier).updateValue(_getIdentifier(this.inputs, _decodeChannel(response)));
+        if (err) {
+          this.accessory.getService(this.device.name).getCharacteristic(Characteristic.ActiveIdentifier).updateValue(err);
+        } else {
+          debug("ActiveIdentifier: Channel Response", _getIdentifier(this.inputs, _decodeChannel(response)));
+          this.accessory.getService(this.device.name).getCharacteristic(Characteristic.ActiveIdentifier).updateValue(_getIdentifier(this.inputs, _decodeChannel(response)));
+        }
       }.bind(this));
     } else {
       debug("inputStatus: Response", response);
